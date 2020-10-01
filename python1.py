7d92bfe0bf591e1283631e0994341ce3d7c60ff1
SYKON77 <58018484+SYKON77@users.noreply.github.com>
2020-10-01 22:24:29 +0530

    python1.py

--- python1.py
+++ python1.py
@@ -1,8 +1,8 @@
 #print string
-print ("sykon is AWESOME")
-print (""" this is a multi 
+print ("SYKON IS AWESOME")
+print (""" This is a multi 
 line string""")
-print ("this is "+"a string")
+print ("This is "+"a string")
 
 #maths
 print(55+45)
@@ -20,11 +20,11 @@ print(quote.lower())
 print(quote.upper())
 print(quote.title())
 ###################################This is boolean expression
-print("This is boolean expression :")
+print("This is boolean expression : ")
 bool1 = "True"
 bool2 = 9 * 9 != "81"
 bool3 = False
-bool4 = 28*21 == 9
+bool4 = 28*4677 == 9
 print(bool1,bool2,bool3,bool4)
 print(type(bool1))
 print(type(bool2))
